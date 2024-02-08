f53f2194d89be2a6fe70ace13bb7b47fee3c4a5e
alvarosac99 <145587906+alvarosac99@users.noreply.github.com>
2024-02-08 19:12:31 +0100

    Primer commit 08/02/24
    Inicio del programa, agregada la función de la maquina y la animación en los botones

---
+++ main.js
@@ -0,0 +1,104 @@
+async function apagar(id) {
+
+    await delay(250);
+    switch (id) {
+        case "verde":
+            document.getElementById('verde').style.backgroundColor = "hsl(120, 100%, 25%)";
+            break;
+        case "rojo":
+            document.getElementById('rojo').style.backgroundColor = "hsl(0, 100%, 25%)";
+            break;
+        case "amarillo":
+            document.getElementById('amarillo').style.backgroundColor = "hsl(60, 100%, 25%)";
+            break;
+        case "azul":
+            document.getElementById('azul').style.backgroundColor = "hsl(240, 100%, 25%)";
+    }
+
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const juego = [
+    ["verde", "rojo"],
+    ["amarillo", "azul"]
+]
+
+function mostrarJuego() {
+    var resultado = document.getElementById("juego");
+    resultado.innerHTML = "";
+
+    for (let i = 0; i < juego.length; i++) {
+        for (let j = 0; j < juego[i].length; j++) {
+            resultado.innerHTML += '<button onclick="clic(event)" class="boton' + '" id="' + juego[i][j] + '"' + '</button>';
+        }
+        resultado.innerHTML += "<br>";
+    }
+
+}
+
+function clic(event) {
+    var id = event.target.id;
+    var boton = event.target;
+    switch (id) {
+        case "verde":
+            boton.style.backgroundColor = "hsla(120, 100%, 50%, 1)";
+            apagar(id);
+            break;
+        case "rojo":
+            boton.style.backgroundColor = "hsla(0, 100%, 50%, 1)";
+            apagar(id);
+            break;
+        case "amarillo":
+            boton.style.backgroundColor = "hsla(60, 100%, 50%, 1)";
+            apagar(id);
+            break;
+        case "azul":
+            boton.style.backgroundColor = "hsla(240, 100%, 50%, 1)";
+            apagar(id);
+            break;
+    }
+}
+
+var array = [];
+
+function IA() {
+    var valor = Math.floor(Math.random() * 4) + 1;
+    switch (valor) {
+        case 1:
+            array.push("verde");
+            break;
+        case 2:
+            array.push("rojo");
+            break;
+        case 3:
+            array.push("amarillo");
+            break;
+        case 4:
+            array.push("azul");
+            break;
+        default:
+            break;
+    }
+}
+
+function juego() {
+    for (let i = 0; i < array.length; i++) {
+        switch (array[i]) {
+            case "verde":
+                document.getElementById('verde').style.backgroundColor = "hsl(120, 100%, 50%)";
+                break;
+            case "rojo":
+                document.getElementById('rojo').style.backgroundColor = "hsl(0, 100%, 50%)";
+                break;
+            case "amarillo":
+                document.getElementById('amarillo').style.backgroundColor = "hsl(60, 100%, 50%)";
+                break;
+            case "azul":
+                document.getElementById('azul').style.backgroundColor = "hsl(240, 100%, 25%)";
+        }
+    }
+}
+
