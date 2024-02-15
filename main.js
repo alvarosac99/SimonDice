97d016602ba3f1f49142296a3a118bbf9296c8c4
alvarosac99 <145587906+alvarosac99@users.noreply.github.com>
2024-02-15 19:36:46 +0100

    Añadido de funciones para que el juego funcione ya.

--- main.js
+++ main.js
@@ -41,40 +41,38 @@ function clic(event) {
     switch (id) {
         case "verde":
             boton.style.backgroundColor = "hsla(120, 100%, 50%, 1)";
-            jugar(id);
-            apagar(id);
             break;
         case "rojo":
             boton.style.backgroundColor = "hsla(0, 100%, 50%, 1)";
-            apagar(id);
             break;
         case "amarillo":
             boton.style.backgroundColor = "hsla(60, 100%, 50%, 1)";
-            apagar(id);
             break;
         case "azul":
             boton.style.backgroundColor = "hsla(240, 100%, 50%, 1)";
-            apagar(id);
             break;
     }
+    apagar(id);
+    jugar(id);
 }
 
-var array = [];
+var maquina = [];
+var jugador = [];
 
 function IA() {
     var valor = Math.floor(Math.random() * 4) + 1;
     switch (valor) {
         case 1:
-            array.push("verde");
+            maquina.push("verde");
             break;
         case 2:
-            array.push("rojo");
+            maquina.push("rojo");
             break;
         case 3:
-            array.push("amarillo");
+            maquina.push("amarillo");
             break;
         case 4:
-            array.push("azul");
+            maquina.push("azul");
             break;
         default:
             break;
@@ -82,9 +80,11 @@ function IA() {
 }
 
 async function juego() {
+    await delay(1000);
     IA();
-    for (let i = 0; i < array.length; i++) {
-        switch (array[i]) {
+    desactivarBotones(true);
+    for (let i = 0; i < maquina.length; i++) {
+        switch (maquina[i]) {
             case "verde":
                 document.getElementById('verde').style.backgroundColor = "hsl(120, 100%, 50%)";
                 break;
@@ -97,14 +97,51 @@ async function juego() {
             case "azul":
                 document.getElementById('azul').style.backgroundColor = "hsl(240, 100%, 50%)";
         }
-        apagar(array[i]);
+        apagar(maquina[i]);
         await delay(500);
+    }
+    jugador = [];
+    desactivarBotones(false);
+}
+
+function jugar(id) {
+
+    switch (id) {
+        case "verde":
+            jugador.push("verde");
+            break;
+        case "rojo":
+            jugador.push("rojo");
+            break;
+        case "amarillo":
+            jugador.push("amarillo");
+            break;
+        case "azul":
+            jugador.push("azul");
+            break;
+    }
+    comprobar();
+}
+
+function comprobar() {
+    for (let i = 0; i < jugador.length; i++) {
+        if (jugador[i] != maquina[i]) {
+            alert("perdiste");
+        }
+    }
+    if (jugador.length == maquina.length) {
+        juego();
+        desactivarBotones(true);
     }
 }
 
-function jugar() {
-
+function desactivarBotones(caso) {
+    var botones = document.getElementsByClassName('boton');
+    for (var i = 0; i < botones.length; i++) {
+        botones[i].disabled = caso;
+    }
 }
+
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
